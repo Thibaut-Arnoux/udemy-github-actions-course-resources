f255cc525855c8a3f5677fd1d41ebc7091792a70
Thibaut-Arnoux <thib.arnoux@gmail.com>
2024-07-16 09:52:59 +0200

    Step 8 - Add input to js action

--- .github/actions/hello-world-javascript/main.js
+++ .github/actions/hello-world-javascript/main.js
@@ -3,7 +3,10 @@ const github = require('@actions/github');
 const exec = require('@actions/exec');
 
 function run() {
-    core.notice('Hello, World from javascript action!');
+    // extract input
+    const input = core.getInput('who-to-greet', { required: true });
+
+    core.notice(`Hello ${input} ! From javascript action!`);
 }
 
 run();

11208b98b8eb1ee6393a39974be70ba57216ed0f
Thibaut-Arnoux <thib.arnoux@gmail.com>
2024-07-16 10:10:45 +0200

    Step 8 - add output for js action

--- .github/actions/hello-world-javascript/main.js
+++ .github/actions/hello-world-javascript/main.js
@@ -6,6 +6,10 @@ function run() {
     // extract input
     const input = core.getInput('who-to-great', { required: true });
 
+    // fill the output
+    const time = Date.now();
+    core.setOutput('time', time.toString());
+
     core.notice(`Hello ${input} ! From javascript action!`);
 }
 
